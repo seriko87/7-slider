9052c82ef409d8bb18cf55fa09cb3061de437269
seriko87 <58387052+seriko87@users.noreply.github.com>
2021-09-18 23:27:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+
+import data from "./data";
 
 function App() {
+  const [user, setUser] = useState(data);
+  const [value, setValue] = useState(0);
+
+  const handleClickLeft = (index1) => {
+    if (index1 === 0) {
+      setValue(user.length - 1);
+    } else {
+      setValue(index1 - 1);
+    }
+  };
+  const handleClickRight = (index2) => {
+    if (index2 === user.length - 1) {
+      setValue(0);
+    } else {
+      setValue(index2 + 1);
+    }
+  };
+
+  const { image, name, title, quote } = user[value];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>
+        {" "}
+        <span>/</span> Reviews
+      </h1>
+      <main>
+        <button onClick={() => handleClickLeft(value)}>
+          <FiChevronLeft />
+        </button>
+        <div className='user'>
+          <img src={image} alt={name} />
+          <h2>{name}</h2>
+          <h3>{title}</h3>
+          <p>{quote}</p>
+        </div>
+        <button onClick={() => handleClickRight(value)}>
+          <FiChevronRight />
+        </button>
+      </main>
+    </section>
   );
 }
 
